fbf4789cf82079255461a7a88532474bd9a4665f
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2023-07-10 22:23:19 +0300

    bugFix

--- components/main/mainSubscribe/main.js
+++ components/main/mainSubscribe/main.js
@@ -10,25 +10,21 @@ function mainSubscribe(){
     document.querySelector('.subscribe').innerHTML = `
     <div class="container"></div>
     `
-    
-    if(currentPage.includes("pages/office")) {
-      mainSubscribeBanner2();
-      mainSubscribeBlock3();
-      mainSubscribeBlock4();
-    } else if (currentPage.includes("/")) {
+    if (currentPage.includes("/")) {
       document.querySelector('.subscribe .container').innerHTML += `
       <h2>Пакет прибирання на місяць</h2>
       <p class="subscribeInfo">Підписка діє протягом 30 днів та лише за однією адресою. При закінченні терміну дія підписки згорає. Працюємо лише по місту Лодзь</p>
       `
       mainSubscribeBanner1();
-      mainSubscribeBanner2();
-      mainSubscribeBlock3();
-      mainSubscribeBlock4();
     }
+
+    mainSubscribeBanner2();
+    mainSubscribeBlock3();
+    mainSubscribeBlock4();
 
     document.querySelector('.subscribe .container').innerHTML += `
     <button class="buySubscribe">Замовити прибирання</button>
     `
     }
     
-    export default mainSubscribe;
+export default mainSubscribe;
